17b360844f452c6d48bc675f4caff043dbf8937a
Mike Kelly <mikekelly321@gmail.com>
2024-09-07 21:13:30 +0100

    Initial Commit

---
+++ SwiftA1/SwiftA1.h
@@ -0,0 +1,18 @@
+//
+//  SwiftA1.h
+//  SwiftA1
+//
+//  Created by Mike Kelly on 07/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftA1.
+FOUNDATION_EXPORT double SwiftA1VersionNumber;
+
+//! Project version string for SwiftA1.
+FOUNDATION_EXPORT const unsigned char SwiftA1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftA1/PublicHeader.h>
+
+
